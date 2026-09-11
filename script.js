ff997600bd55dae7b324f6f9fa357282a697495f
c20245402andales-sudo <c20245402.andales@csav.edu.ph>
2026-09-11 10:13:27 +0800

    functions

--- script.js
+++ script.js
@@ -9,3 +9,26 @@ console.log (`Name: ${myName}`);
 console.log (`Age: ${age}`);
 console.log (`Number: ${number}`);
 console.log (`Address: ${address}`);
+
+function greet(name) {
+    return `Good morning, ${name}`;
+
+}
+console.log(greet(`Rene`)); 
+
+
+ 
+
+function add (a,b) {
+    return a + b;
+}
+function mdas (num1,num2) {
+    let m = num1 * num2;
+    let d = num1 / num2;
+    let a = num1 + num2;
+    let s = num1 - num2;
+    return (`values: ${num1} and ${num2},\n product: ${m}, \n quotient: ${d}, \n sum: ${a}, \n difference: ${s}`);
+    
+    }
+   
+   console.log (mdas(5,3));
