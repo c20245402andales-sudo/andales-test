1fa8113235a8d389663af9dfec6d5f6d06e55be0
c20245402andales-sudo <c20245402.andales@csav.edu.ph>
2026-09-11 12:08:11 +0800

    .style

--- script.js
+++ script.js
@@ -1,46 +1,83 @@
-    console.log ("Hello, world!");
-    const myName = "Elmer Andales";
+    console.log("Hello World");
+    const myName = "john derence aposaga";
     let age = 20;
-    const number = "9052418309";
-    const address = "De leon st. bryg2";
+    const number = 9163214632;
+    const address = "victorias city brgy 3 bat-us";
+
+    console.log(`Name: ${myName}`);
+    console.log(`Age: ${age}`);
+    console.log(`Number: ${number}`);
+    console.log(`Adress: ${address}`);
 
 
-    console.log (`Name: ${myName}`);
-    console.log (`Age: ${age}`);
-    console.log (`Number: ${number}`);
-    console.log (`Address: ${address}`);
+    //functions 
 
     function greet(name) {
-        return `Good morning, ${name}`;
+
+        return `good morning, ${name}`;
+
 
     }
-    console.log(greet(`Rene`)); 
+    console.log(greet("Rene"));
 
 
-    
-
-    function add (a,b) {
+    //function 
+    function add(a, b) {
         return a + b;
     }
-    function mdas (num1,num2) {
+    function mdas(num1, num2) {
         let m = num1 * num2;
         let d = num1 / num2;
         let a = num1 + num2;
         let s = num1 - num2;
-        return (`values: ${num1} and ${num2},\n product: ${m}, \n quotient: ${d}, \n sum: ${a}, \n difference: ${s}`);
+        return (`values: ${num1} and ${num2}\n product: ${m}\n quotient: ${d}\n sum: ${a}\n difference: ${s}`);
 
-        }
 
-    console.log (mdas(5,3));
+    }
+
+    console.log(mdas(5, 3));
+
+    //query selector
 
     const heading = document.querySelector("h1");
     console.log(heading);
+
     const contactHeading = document.querySelector("#contact h2");
     console.log(contactHeading);
+
+    const projectsHeading = document.querySelector("#projects h2");
+    console.log(projectsHeading);
     const servicesHeading = document.querySelector("#services h2");
     console.log(servicesHeading);
 
+    //text Content
     heading.textContent = "My Portfolio";
-    contactHeading.textContent = "Lets connect";
-    projectHeading.textContent = "My Projects";
-    
+    contactHeading.textContent = "lets connect";
+    projectsHeading.textContent = "My Projects";
+    servicesHeading.textContent = "My Services";
+
+    //.style
+    heading.style.color = "purple";
+    heading.style.backgroundColor = "yellow";
+    contactHeading.style.color = "purple";
+    projectsHeading.style.color = "purple";
+    servicesHeading.style.color = "purple";
+
+    heading.addEventListener("click", function () {
+        heading.style.color = "red";
+    }
+
+    );
+    const toggleButton = document.querySelector('#switch');
+    const body = document.querySelector('body');
+    let isOff = false;
+
+    toggleButton.addEventListener("click", function () {
+        isOff = !isOff;
+
+        if (isOff) {
+            
+        } else {
+            
+        }
+    });
