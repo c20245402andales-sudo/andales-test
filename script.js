36cd75f853248d3618672cb9b121a8fe36f98381
c20245402andales-sudo <c20245402.andales@csav.edu.ph>
2026-09-11 12:11:16 +0800

    addEventListener

--- script.js
+++ script.js
@@ -76,8 +76,11 @@
         isOff = !isOff;
 
         if (isOff) {
+            body.style.backgroundColor = "black";
+            body.style.color = "white";
             
         } else {
-            
+            body.style.backgroundColor = "white";
+            body.style.color = "black";
         }
     });
