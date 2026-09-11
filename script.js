73f3085f5c6264ea297153b985055056759cbb98
c20245402andales-sudo <c20245402.andales@csav.edu.ph>
2026-09-11 09:18:26 +0800

    variables (let & const)

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log ("Hello, world!");
+const myName = "Elmer Andales";
+let age = 20;
+const number = "9052418309";
+const address = "De leon st. bryg2";
+
+
+console.log (`Name: ${myName}`);
+console.log (`Age: ${age}`);
+console.log (`Number: ${number}`);
+console.log (`Address: ${address}`);
