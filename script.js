576d61e27a08f8f6db9d5dcc35d9dd0dcf367cf4
c20245402andales-sudo <c20245402.andales@csav.edu.ph>
2026-09-11 11:16:00 +0800

    querySelector

--- script.js
+++ script.js
@@ -1,34 +1,42 @@
-console.log ("Hello, world!");
-const myName = "Elmer Andales";
-let age = 20;
-const number = "9052418309";
-const address = "De leon st. bryg2";
+    console.log ("Hello, world!");
+    const myName = "Elmer Andales";
+    let age = 20;
+    const number = "9052418309";
+    const address = "De leon st. bryg2";
 
 
-console.log (`Name: ${myName}`);
-console.log (`Age: ${age}`);
-console.log (`Number: ${number}`);
-console.log (`Address: ${address}`);
+    console.log (`Name: ${myName}`);
+    console.log (`Age: ${age}`);
+    console.log (`Number: ${number}`);
+    console.log (`Address: ${address}`);
 
-function greet(name) {
-    return `Good morning, ${name}`;
+    function greet(name) {
+        return `Good morning, ${name}`;
 
-}
-console.log(greet(`Rene`)); 
-
-
- 
-
-function add (a,b) {
-    return a + b;
-}
-function mdas (num1,num2) {
-    let m = num1 * num2;
-    let d = num1 / num2;
-    let a = num1 + num2;
-    let s = num1 - num2;
-    return (`values: ${num1} and ${num2},\n product: ${m}, \n quotient: ${d}, \n sum: ${a}, \n difference: ${s}`);
-    
     }
-   
-   console.log (mdas(5,3));
+    console.log(greet(`Rene`)); 
+
+
+    
+
+    function add (a,b) {
+        return a + b;
+    }
+    function mdas (num1,num2) {
+        let m = num1 * num2;
+        let d = num1 / num2;
+        let a = num1 + num2;
+        let s = num1 - num2;
+        return (`values: ${num1} and ${num2},\n product: ${m}, \n quotient: ${d}, \n sum: ${a}, \n difference: ${s}`);
+
+        }
+
+    console.log (mdas(5,3));
+
+    const heading = document.querySelector("h1");
+    console.log(heading);
+    const contactHeading = document.querySelector("#contact h2");
+    console.log(contactHeading);
+    const servicesHeading = document.querySelector("#services h2");
+    console.log(servicesHeading);
+    
